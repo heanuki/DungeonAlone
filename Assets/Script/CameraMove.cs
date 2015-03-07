3d269a8d60f30079969ec89c3da3efce9f8fe701
innocentree <innocent.tree@gmail.com>
2015-03-08 00:25:40 +0900

    메인카메라 이동 적용

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -7,10 +7,11 @@ public class CameraMove : MonoBehaviour {
     public Vector3 endPos;
     public Transform endPosTrans;
     public float speed = 1f;
+    public float offsetToChar = 2f;
     void Awake()
     {
         startPos = transform.position;
-        endPos = endPosTrans.position;
+        endPos = endPosTrans.position - new Vector3(0f, 0f, offsetToChar);
     }
 	// Update is called once per frame
 	void Update () 
