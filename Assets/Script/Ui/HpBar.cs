2c9ffb23a2972a1bebac4b9cb71052094e0b1995
Kyeongho Park <kpark_ms@hotmail.com>
2015-03-08 00:22:53 +0900

    UI: HP 게이지와 열쇠 아이콘 추가

---
+++ Assets/Script/Ui/HpBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HpBar : MonoBehaviour {
+    public Image hpValue;
+    js_Status playerStatus;
+
+    void Awake() {
+        playerStatus = GameObject.Find("Player").GetComponent<js_Status>();
+    }
+
+	void Update () {
+        hpValue.fillAmount = (float) js_Status.healthPoint / (float) js_Status.maxHealthPoint;
+    }
+}
---
+++ Assets/Script/Ui/KeyIcon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyIcon : MonoBehaviour {
+    public GameObject[] keyIcons;
+    public FSMPlayerData playerData;
+
+    int prevKeysSize;
+
+    void Awake() {
+        playerData = GameObject.Find("Player").GetComponent<FSMPlayerData>();
+        prevKeysSize = 0;
+    }
+
+    void Update() {
+        int keysSize = playerData.keyNum;
+        if (keysSize == prevKeysSize) {
+            return;
+        }
+
+        int displaySize = Mathf.Min(keysSize, keyIcons.Length);
+        int i = 0;
+        for (; i < displaySize; ++i) {
+            keyIcons[i].SetActive(true);
+        }
+        for (; i < keyIcons.Length; ++i) {
+            keyIcons[i].SetActive(false);
+        }
+	}
+}
