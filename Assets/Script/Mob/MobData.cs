6a7dc9842fed1dfc6c1b0731c6aac75f95019e4b
Kyeongho Park <kpark_ms@hotmail.com>
2015-03-07 20:43:10 +0900

    몹 이동 관련 옵션을 에디터에서 설정하도록 변경.
    moveSpeed, visualRange.

--- Assets/Script/Mob/MobData.cs
+++ Assets/Script/Mob/MobData.cs
@@ -9,18 +9,16 @@ public class MobData : MonoBehaviour {
     public GameObject target;
 
     //  몹의 속도
-    public float moveSpeed;
+    public float moveSpeed = 1f;
 
     // 몹의 가시 거리
-    public float visualRange;
+    public float visualRange = 3f;
 
     // 몹이 이동하려는 목표 위치
     public float xToMove;
     public float zToMove;
 
     void Awake() {
-        moveSpeed = 1f;
-        visualRange = 3f;
         xToMove = Mathf.Round(mob.transform.position.x);
         zToMove = Mathf.Round(mob.transform.position.z);
     }
