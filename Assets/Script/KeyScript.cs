c362f81f35fc06d86b3f7b3c28315538a05e5292
innocentree <innocent.tree@gmail.com>
2015-03-07 23:30:11 +0900

    키 먹으면 사라지도록 변경

--- Assets/Script/KeyScript.cs
+++ Assets/Script/KeyScript.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class KeyScript : MonoBehaviour 
 {
+    public float turnspeed = 50f;
     void OnTriggerEnter(Collider col)
     {
         Debug.Log("getkey");
         audio.Play();
         col.transform.GetComponent<FSMPlayerData>().keyNum++;
+        Destroy(gameObject);
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 1f, 0f), Time.deltaTime * turnspeed);
+       // transform.Translate(0f, Mathf.Sin(Time.deltaTime), 0f);
     }
 
 }
