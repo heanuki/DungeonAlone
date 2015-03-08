54e4c0ca2ebfd474bb5f38349905595e6c263d44
innocentree <innocent.tree@gmail.com>
2015-03-08 13:32:28 +0900

    말풍선 추가

---
+++ Assets/Script/BalloonDisabled.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BalloonDisabled : MonoBehaviour {
+
+    public float EnabledTime = 1f;
+    public  float _t;
+    public GameObject btn;
+
+    void Awake()
+    {
+        _t = EnabledTime;
+    }
+	// Update is called once per frame
+	void Update () 
+    {
+        if (transform.localScale.x > 0f)
+        {
+            if (_t < 0f)
+            {
+                transform.localScale = new Vector3(0f, 0f, 0f);
+                _t = EnabledTime;
+            }
+            _t -= Time.deltaTime;
+        }
+	}
+}
---
+++ Assets/Script/NearbyMob.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NearbyMob : MonoBehaviour 
+{
+    public GameObject temp;
+    public GameObject pc;
+    public float damageCycle = 1f;
+    void Awake()
+    {
+        temp = GameObject.FindGameObjectWithTag("Balloon") ;
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            //temp.SetActive(true);
+            temp.transform.localScale = new Vector3(1f, 1f, 1f);
+            pc = col.transform.gameObject;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            temp.transform.localScale = new Vector3(0f, 0f, 0f);
+            pc = null;
+        }
+    }
+    void Update()
+    {
+        if(pc != null)
+        {
+            if (damageCycle < 0f)
+            {
+               // Debug.Log("damaged!!!");
+                pc.SendMessage("Damaged", 3, SendMessageOptions.RequireReceiver);
+                damageCycle = 2f;
+            }
+            damageCycle -= Time.deltaTime;
+            
+        }
+    }
+}
